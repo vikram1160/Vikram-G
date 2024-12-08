0ed8726622ab1568850304675554fbf90e0d236e
vikram1160 <global-vgeditor12@gmail.com>
2024-12-09 00:11:42 +0530

    add btn toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink,NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
