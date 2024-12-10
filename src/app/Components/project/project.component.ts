0f90670d7bb927368dd7c1cf574cbefb70af416a
vikram1160 <global-vgeditor12@gmail.com>
2024-12-11 00:14:41 +0530

    add footer

--- src/app/Components/project/project.component.ts
+++ src/app/Components/project/project.component.ts
@@ -9,27 +9,8 @@ import { Component } from '@angular/core';
 })
 export class ProjectComponent {
 
-  projects = [
-    {
-      title: 'Youtube Clone',
-      description: 'Created a YouTube clone to implement my knowledge in React.'
-    },
-    {
-      title: 'Library Management System',
-      description: 'Built a project to digitize library operations using modern technologies.'
-    },
-    {
-      title: 'Resume Creator',
-      description: 'Developed a tool to help users create professional resumes efficiently.'
-    },
-    {
-      title: 'Apple Clone',
-      description: 'Created an Apple website clone to showcase frontend skills.'
-    }
-  ];
-
-  onReadMore(title: string): void {
-    alert(`Read more about ${title}`);
+  onReadMore(): void {
+    alert('I Am Working In Web Application');
   }
 
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./Components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink,NgClass],
+  imports: [RouterOutlet, RouterLink, NgClass, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
