357b920e2b2af859d0eeb39880c8de830e6a3a2b
vikram1160 <global-vgeditor12@gmail.com>
2024-12-10 23:26:51 +0530

    technologies done

--- src/app/Components/technologies/technologies.component.ts
+++ src/app/Components/technologies/technologies.component.ts
@@ -4,31 +4,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-technologies',
   standalone: true,
-  imports: [NgFor ],
+  imports: [ ],
   templateUrl: './technologies.component.html',
   styleUrl: './technologies.component.css'
 })
 export class TechnologiesComponent {
 
-  technicalSkills = [
-    { name: 'HTML', percentage: '93%' },
-    { name: 'CSS', percentage: '87%' },
-    { name: 'JavaScript', percentage: '72%' },
-    { name: 'Bootstrap', percentage: '81%' },
-    { name: 'Angular', percentage: '57%' },
-    { name: 'React', percentage: '78%' },
-    { name: 'MySQL', percentage: '69%' },
-    { name: 'Java', percentage: '40%' },
+  skills = [
+    { name: 'ANGULAR', percentage: 57 }
   ];
-
-  personalSkills = [
-    'Installation and configuration of various operating systems (Windows 7, 8.1, 10, 11).',
-    'Support for MS Office 365, Outlook MS Office & Antivirus.',
-    'Laptop Parts Exchange & Repair.',
-    'Diagnose hardware issues and raise tickets for hardware replacement.',
-    'Installation & troubleshooting of hardware and replacement of desktop/laptop parts.',
-    'All laptop formatting services.',
-  ];
-skills: any;
 
 }
